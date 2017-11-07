d5f5f7d19d739fcace8e5ab83f9f7fc87b6e384c
David Meir-Levy <davidmeirlevy@gmail.com>
2017-11-07 15:05:34 +0200

    compile module as separated from main app

--- template/src/main.ts
+++ template/src/main.ts
@@ -2,22 +2,15 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import './sass/main.scss';
-
-import { HomeComponent } from './components/home';
-import { AboutComponent } from './components/about';
-import { ListComponent } from './components/list';
-import { NavbarComponent } from './components/navbar';
+import {routes} from './module';
+import {NavbarComponent} from './components/navbar';
 
 // register the plugin
 Vue.use(VueRouter);
 
 let router = new VueRouter({
   mode: 'history',
-  routes: [
-    { path: '/', component: HomeComponent },
-    { path: '/about', component: AboutComponent },
-    { path: '/list', component: ListComponent },
-  ]
+  routes: routes('')
 });
 
 new Vue({
---
+++ template/src/module.ts
@@ -0,0 +1,22 @@
+import RouteConfig from 'vue-router';
+import {HomeComponent} from './components/home';
+import {AboutComponent} from './components/about';
+import {ListComponent} from './components/list';
+
+const routesList = [
+  {path: '/', component: HomeComponent},
+  {path: '/about', component: AboutComponent},
+  {path: '/list', component: ListComponent},
+];
+
+/**
+ *
+ * @param {string} prefix
+ * @returns RouteConfig[]}
+ */
+export function routes(prefix: string = '') {
+  return routesList.map(route => ({
+    ...route,
+    path: prefix + route.path
+  }));
+}
