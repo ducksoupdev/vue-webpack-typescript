1c1e20b16c7f5f840aa4ecaa710121080aec9b8c
Johannes Feig <johannes.feig@leadin.fi>
2018-01-07 17:47:50 +0200

    router.ts: replaced static router with dynamic router

--- template/src/router.ts
+++ template/src/router.ts
@@ -2,45 +2,48 @@ import Vue from 'vue';
 import VueRouter, { Location, Route, RouteConfig } from 'vue-router';
 import { makeHot, reload } from './util/hot-reload';
 
-const homeComponent = () => import('./components/home').then(({ HomeComponent }) => HomeComponent);
-const aboutComponent = () => import('./components/about').then(({ AboutComponent }) => AboutComponent);
-const listComponent = () => import('./components/list').then(({ ListComponent }) => ListComponent);
-// const homeComponent = () => import(/* webpackChunkName: 'home' */'./components/home').then(({ HomeComponent }) => HomeComponent);
-// const aboutComponent = () => import(/* webpackChunkName: 'about' */'./components/about').then(({ AboutComponent }) => AboutComponent);
-// const listComponent = () => import(/* webpackChunkName: 'list' */'./components/list').then(({ ListComponent }) => ListComponent);
-
-if (process.env.ENV === 'development' && module.hot) {
-  const homeModuleId = './components/home';
-  const aboutModuleId = './components/about';
-  const listModuleId = './components/list';
-
-  // first arguments for `module.hot.accept` and `require` methods have to be static strings
-  // see https://github.com/webpack/webpack/issues/5668
-  makeHot(homeModuleId, homeComponent,
-    module.hot.accept('./components/home', () => reload(homeModuleId, (<any>require('./components/home')).HomeComponent)));
-
-  makeHot(aboutModuleId, aboutComponent,
-    module.hot.accept('./components/about', () => reload(aboutModuleId, (<any>require('./components/about')).AboutComponent)));
-
-  makeHot(listModuleId, listComponent,
-    module.hot.accept('./components/list', () => reload(listModuleId, (<any>require('./components/list')).ListComponent)));
+interface IRouteComponentConfiguration {
+  componentName: string;
+  componentPath: string;
+  urlPath: string;
 }
+
+const routeComponentConfigurationList: IRouteComponentConfiguration[] = [
+  {
+    'componentName': 'HomeComponent',
+    'componentPath': './components/home',
+    'urlPath': '/'
+  },
+  {
+    'componentName': 'AboutComponent',
+    'componentPath': './components/about',
+    'urlPath': '/about'
+  },
+  {
+    'componentName': 'ListComponent',
+    'componentPath': './components/list',
+    'urlPath': '/list'
+  },
+];
+
+let routeComponentList = [];
+
+routeComponentConfigurationList.forEach((routeComponentConfiguration: IRouteComponentConfiguration) => {
+  // add empty string is necessary to get import to work
+  let routeComponent = {
+    'component': () => import('' + routeComponentConfiguration.componentPath).then((Component) => Component[routeComponentConfiguration.componentName]),
+    'path': routeComponentConfiguration.urlPath
+  };
+  
+  routeComponentList.push(routeComponent);
+  if (process.env.ENV === 'development' && module.hot) {
+    makeHot(routeComponentConfiguration.componentPath, routeComponent.component,
+      module.hot.accept(routeComponentConfiguration.componentPath, () => reload(routeComponentConfiguration.componentPath, (<any>require(routeComponentConfiguration.componentPath))[routeComponentConfiguration.componentName])));
+  }
+});
 
 Vue.use(VueRouter);
 
-export const createRoutes: () => RouteConfig[] = () => [
-  {
-    path: '/',
-    component: homeComponent,
-  },
-  {
-    path: '/about',
-    component: aboutComponent,
-  },
-  {
-    path: '/list',
-    component: listComponent,
-  }
-];
+export const createRoutes: () => RouteConfig[] = () => routeComponentList;
 
 export const createRouter = () => new VueRouter({ mode: 'history', routes: createRoutes() });
